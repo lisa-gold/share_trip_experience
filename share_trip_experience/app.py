b104b69eef5965b576553736d1c8f5fe76787932
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2024-06-12 14:50:26 +0300

    add registration

--- share_trip_experience/app.py
+++ share_trip_experience/app.py
@@ -1,12 +1,56 @@
 import os
+from dotenv import load_dotenv
 from flask import (
     Flask,
-    render_template)
+    render_template,
+    url_for,
+    request,
+    flash,
+    redirect)
+from pymongo import MongoClient
+
+
+load_dotenv()
+
+
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+DB = os.getenv("DB")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
+try:
+    # todo: test mode
+    cluster = MongoClient(f"mongodb+srv://{USERNAME}:{PASSWORD}@{DB}")
+    db = cluster['trip-share']
+except Exception as e:
+    print('no connection with the db ', e)
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.get('/registration')
+def registration_form():
+    return render_template('registration.html')
+
+
+@app.post('/registration')
+def add_user():
+    # todo validation (unique, password length, etc.), encryption
+    user = {
+        'name': request.form['name'],
+        'password': request.form['password']
+    }
+    db['users'].insert_one(user)
+    flash('User is added successfully', 'alert alert-success')
+    return redirect(url_for('get_my_trips'), code=302)
+
+
+@app.get('/my_trips')
+def get_my_trips():
+    return render_template('my_trips.html')
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,3 +6,24 @@ def test_home_route():
 
     assert response.status_code == 200
     assert 'Welcome!' in response.text
+
+
+def test_get_registration():
+    response = app.test_client().get('/registration')
+
+    assert response.status_code == 200
+    assert 'Name' in response.text
+
+
+def test_post_registration():
+    new_user = {
+        'name': 'test',
+        'password': '123'
+    }
+    response = app.test_client().post('/registration', data={
+        'name': new_user['name'],
+        'password': new_user['password']})
+
+    # todo: test validation
+
+    assert response.status_code == 302
